01e36c5af769f951aca1e938588631a78faf7f09
Claire <claire.cy.t@gmail.com>
2022-04-19 22:15:45 -0400

    add migration

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     is_ambassador = models.BooleanField(default=True)
     username = None
