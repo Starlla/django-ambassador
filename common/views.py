6a61c3901b018bd19a74032bb69eda390c06a098
Claire <claire.cy.t@gmail.com>
2022-04-21 00:41:51 -0400

    add urls

--- common/views.py
+++ common/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
 # Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, requet):
+        return Response("Hello")
