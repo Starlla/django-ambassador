88180fa3ce6e9291cfc26fd8c7c6bf38b1e00a84
Claire <claire.cy.t@gmail.com>
2022-04-21 11:20:14 -0400

    finish user view

--- common/views.py
+++ common/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from rest_framework import exceptions
+
+from .serializers import UserSerializer
 
 # Create your views here.
 class RegisterAPIView(APIView):
-    def post(self, requet):
-        return Response("Hello")
+    def post(self, request):
+        data = request.data
+        if data["password"] != data["password_confirm"]:
+            raise exceptions.APIException("Password do not match!")
+        data["is_ambassador"] = 0
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
