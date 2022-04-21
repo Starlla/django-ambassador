adc950fb8472fa05fc8fb1f32688163f6dee0c14
Claire <claire.cy.t@gmail.com>
2022-04-20 23:53:31 -0400

    add user model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Usesr must have an email")
+        if not password:
+            raise ValueError("user must have a password")
+        user = serlf.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_admin = False
+        user.is_staff = False
+        user.save(self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError("Usesr must have an email")
+        if not password:
+            raise ValueError("user must have a password")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(self._db)
+        return user
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -12,3 +41,5 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
